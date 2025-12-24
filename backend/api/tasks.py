ad6396b9a9a35950f239a07b467179312823bc97
aashtiany <amir.klavuhn-ashtiany@dodiis.mil>
2025-12-24 14:21:00 -0500

    feat: chain solr indexing task after audit log persistence

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -6,9 +6,29 @@ from celery import shared_task
 def log_task():
     my_uuid = uuid.uuid4()
     print(f"Task logged! id {my_uuid}")
-    
+
+
 @shared_task(name="api.tasks.audit_log")
 def audit_log(event_type, message, request_id, source):
     from api.models import AuditLog
-    AuditLog.objects.create(event_type=event_type, message=message, request_id=request_id, source=source)
-    
+
+    new_audit_log = AuditLog.objects.create(
+        event_type=event_type, message=message, request_id=request_id, source=source
+    )
+
+    index_audit_log.delay(new_audit_log.id)
+
+
+@shared_task(name="api.tasks.index_audit_log")
+def index_audit_log(audit_log_id):
+    from api.models import AuditLog
+
+    audit_log = AuditLog.objects.get(id=audit_log_id)
+    document = {
+        "id": audit_log_id,
+        "event_type": audit_log.event_type,
+        "message": audit_log.message,
+        "request_id": audit_log.request_id,
+        "source": audit_log.source,
+        "created_at": audit_log.created_at.isoformat(),
+    }
