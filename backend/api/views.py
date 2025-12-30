2dcca8a0f13b6509fa42758724bc8ed08b985b1b
aashtiany <amir.klavuhn-ashtiany@dodiis.mil>
2025-12-30 13:56:58 -0500

    fix: removed imports that affected health check conneection

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,15 @@
 import logging
 import uuid
 from django.http import JsonResponse
-from .tasks import audit_log
 from api.health.database import check_database_health
 
 logger = logging.getLogger(__name__)
 
 
 def health_check(request):
-    uuid_object = uuid.uuid4()
-    request_id = str(uuid_object)
+    request_id = str(uuid.uuid4())
     logger.info("health_check_called", extra={"request_id": request_id})
-    
+
     if check_database_health():
         return JsonResponse({"status": "ok", "request_id": request_id})
     return JsonResponse({"status": "unhealthy"}, status=503)
