7176c4c3eae24ecf66c091745629e50025594785
aashtiany <amir.klavuhn-ashtiany@dodiis.mil>
2025-12-31 14:30:53 -0500

    feat: updating env to aws

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,33 @@
 set -e
 
 echo "Starting deployment..."
+export AWS_DEFAULT_REGION=us-east-1
+
+get_ssm() {
+    local name="$1"
+
+    aws ssm get-parameter \
+    --name "$name" \
+    --with-decryption \
+    --query "Parameter.Value" \
+    --output text
+}
+
+ALLOWED_HOSTS=$(get_ssm "/mission-intake/prod/ALLOWED_HOSTS")
+DJANGO_SECRET_KEY=$(get_ssm "/mission-intake/prod/DJANGO_SECRET_KEY")
+POSTGRES_DB=$(get_ssm "/mission-intake/prod/POSTGRES_DB")
+POSTGRES_PASSWORD=$(get_ssm "/mission-intake/prod/POSTGRES_PASSWORD")
+POSTGRES_PORT=$(get_ssm "/mission-intake/prod/POSTGRES_PORT")
+POSTGRES_USER=$(get_ssm "/mission-intake/prod/POSTGRES_USER")
+SOLR_URL=$(get_ssm "/mission-intake/prod/SOLR_URL")
+
+export ALLOWED_HOSTS
+export DJANGO_SECRET_KEY
+export POSTGRES_DB
+export POSTGRES_PASSWORD
+export POSTGRES_PORT
+export POSTGRES_USER
+export SOLR_URL
 
 echo "Pulling latest code"
 git pull origin main
