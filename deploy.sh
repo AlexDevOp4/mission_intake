ce453ce892e2335406047a13f919e112a26ed606
aashtiany <amir.klavuhn-ashtiany@dodiis.mil>
2025-12-31 14:43:13 -0500

    fix: fixed shell errors

--- deploy.sh
+++ deploy.sh
@@ -14,21 +14,13 @@ get_ssm() {
     --output text
 }
 
-ALLOWED_HOSTS=$(get_ssm "/mission-intake/prod/ALLOWED_HOSTS")
-DJANGO_SECRET_KEY=$(get_ssm "/mission-intake/prod/DJANGO_SECRET_KEY")
-POSTGRES_DB=$(get_ssm "/mission-intake/prod/POSTGRES_DB")
-POSTGRES_PASSWORD=$(get_ssm "/mission-intake/prod/POSTGRES_PASSWORD")
-POSTGRES_PORT=$(get_ssm "/mission-intake/prod/POSTGRES_PORT")
-POSTGRES_USER=$(get_ssm "/mission-intake/prod/POSTGRES_USER")
-SOLR_URL=$(get_ssm "/mission-intake/prod/SOLR_URL")
-
-export ALLOWED_HOSTS
-export DJANGO_SECRET_KEY
-export POSTGRES_DB
-export POSTGRES_PASSWORD
-export POSTGRES_PORT
-export POSTGRES_USER
-export SOLR_URL
+export ALLOWED_HOSTS=$(get_ssm "/mission-intake/prod/ALLOWED_HOSTS")
+export DJANGO_SECRET_KEY=$(get_ssm "/mission-intake/prod/DJANGO_SECRET_KEY")
+export POSTGRES_DB=$(get_ssm "/mission-intake/prod/POSTGRES_DB")
+export POSTGRES_PASSWORD=$(get_ssm "/mission-intake/prod/POSTGRES_PASSWORD")
+export POSTGRES_PORT=$(get_ssm "/mission-intake/prod/POSTGRES_PORT")
+export POSTGRES_USER=$(get_ssm "/mission-intake/prod/POSTGRES_USER")
+export SOLR_URL=$(get_ssm "/mission-intake/prod/SOLR_URL")
 
 echo "Pulling latest code"
 git pull origin main
